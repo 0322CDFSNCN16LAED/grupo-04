b0115618ba32bb691e491a222179fd71400e5980
PieroJacinto <pierojacinto@gmail.com>
2022-05-09 20:28:23 -0300

    creando entorno de node js y estructura base de la pagina

---
+++ app.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Estamos corriendo en el puerto " + PORT);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  
+    res.sendFile(path.join(__dirname, "views/home.html"));
+});
