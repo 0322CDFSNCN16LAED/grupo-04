d65350c9a722c3b44d3818a868494141e80541a6
PieroJacinto <pierojacinto@gmail.com>
2022-05-09 20:30:15 -0300

    cambie el nombre homea  index.html

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   
-    res.sendFile(path.join(__dirname, "views/home.html"));
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
