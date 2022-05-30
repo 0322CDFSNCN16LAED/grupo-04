b68aff57de9f0f0cf1e837f34d9a2c25d2906de4
Julieta Asis Reck <juliasisrueck@hotmail.com>
2022-05-30 11:49:55 -0300

    Ordené las lineas de app.js y corregí la opacidad del form de register

--- app.js
+++ app.js
@@ -1,31 +1,20 @@
 const path = require("path");
 const express = require("express");
-
 const app = express();
 
-const PORT = 3000;
+const mainRouter = require("./routes/main.js");
+const productsRouter = require ("./routes/products.js");
+const budgetRouter = require("./routes/budget.js");
 
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Estamos corriendo en el puerto " + PORT);
 });
 
-const mainRouter = require("./routes/main.js");
-
-const productsRouter = require ("./routes/products.js");
-
-const budgetRouter = require("./routes/budget.js");
+app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", mainRouter);
-
 app.use("/product", productsRouter);
-
 app.use("/budget", budgetRouter);
-
-
-
-
-
-
-app.set("view engine", "ejs");
