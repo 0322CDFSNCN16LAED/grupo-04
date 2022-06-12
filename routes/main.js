365099b33492d20d779c06aceded52a6cff93d92
PieroJacinto <pierojacinto@gmail.com>
2022-06-12 13:09:31 -0300

    routes actualized and  start added to package.json scripts

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const mainControllers = require("../controllers/main-controllers");
+const productsRouter = require("../routes/products.js");
+const budgetRouter = require("../routes/budget.js");
+const registerRouter = require("../routes/register.js");
+
+router.use("/product", productsRouter);
+router.use("/budget", budgetRouter);
+router.use("/register", registerRouter);
 
 router.get("/", mainControllers.home);
 router.get("/login", mainControllers.login);
