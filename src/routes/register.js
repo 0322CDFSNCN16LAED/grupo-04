2d7389ef95758f7df98370f84fde379bfefc685f
pieroJacinto <pierojacinto@gmail.com>
2022-06-18 12:01:48 -0300

    registerprof prepared

--- src/routes/register.js
+++ src/routes/register.js
@@ -53,6 +53,38 @@ const userValidations = [
     })
 ];
 
+const ProfValidations = [
+  body("name").notEmpty().withMessage("Debes introducir un nombre"),
+  body("lastName").notEmpty().withMessage("Debes introducir un apellido"),
+  body("userName").notEmpty().withMessage("Debes introducir un nombre de usuario"),
+  body("email")
+    .notEmpty().withMessage("Debes introducir un email").bail()
+    .isEmail().withMessage("Debes escribir un formato de correo válido"),
+  body("password").notEmpty().withMessage("Debes introducir una contraseña"),
+  body("phone").notEmpty().withMessage("Debes introducir un teléfono"),
+  body("DNI").notEmpty().withMessage("Debes introducir un DNI"),
+  body("IVA").notEmpty().withMessage("Debes introducir una condición frente al IVA"),
+  body("CUIT").notEmpty().withMessage("Debes introducir un número de CUIT"),
+  body("rubro").notEmpty().withMessage("Debes introducir un rubro"),
+  body("time").notEmpty().withMessage("Debes introducir un horario disponible"),  
+  body("avatar").custom((value, { req }) => {
+    const file = req.file;
+    const acceptedExtensions = [".gif",".png",".tif",".jpg"];
+    
+    if(!file) {
+      throw new Error("Debes subir una imagen de perfil")
+    }else {
+      const fileExtension = path.extname(file.originalname);
+      if(!acceptedExtensions.includes(fileExtension)){
+        throw new Error(`Las extensiones de archivo permitidas son: ${acceptedExtensions.join(", ")}`);
+      }
+    }
+    return true
+  })
+];
+
+
+
 router.get("/", registerControllers.register);
 
 //*create and store user*//
@@ -66,6 +98,7 @@ router.post("/prof",
     { name: "avatar", maxCount: 1 },
     { name: "finished-jobs", maxCount: 5 }
   ]),
+  ProfValidations,
   registerControllers.storeProf);
 
 module.exports = router;
