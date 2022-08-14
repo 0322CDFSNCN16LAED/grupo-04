b4f743e4b733cb0b909d8afc392c277ca8466f1e
PieroJacinto <pierojacinto@gmail.com>
2022-08-14 00:20:43 -0300

    corousel arrows color changed

--- public/js/registerProf.js
+++ public/js/registerProf.js
@@ -1,11 +1,3 @@
 window.onload = async () => {
-  const checkbox = document.querySelector("#rubro");
 
-
-  checkbox.addEventListener("click", async () => {
-    if (checkbox.checked){
-        checkbox.innertext = "checked"
-    }
-    
-  });
 };
