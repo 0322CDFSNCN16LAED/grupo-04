226f2923b4a92c000310b280ad6b1dd54c414315
PieroJacinto <pierojacinto@gmail.com>
2022-09-03 21:15:14 -0300

    small changes maded to cart validations

--- src/middlewares/cartValidations.js
+++ src/middlewares/cartValidations.js
@@ -4,16 +4,16 @@ const dayjs = require("dayjs");
 module.exports = {
   cartValidations: [
     body("diaTurno").custom((value, { req }) => {
-      console.log(today, req.body);
       var today = dayjs(new Date()).format("YYYY-MM-DD");
       var inputDate = req.body.diaTurno;
       const valid = today < inputDate;
-      console.log(valid);
       if (valid == false) {
         throw new Error("Debes seleccionar una fecha valida");
       }
       return true;
     }),
-    body("metodoPago").notEmpty().withMessage("debes seleccionar un metodo de pago"),
+    body("metodoPago")
+      .notEmpty()
+      .withMessage("debes seleccionar un metodo de pago"),
   ],
 };
