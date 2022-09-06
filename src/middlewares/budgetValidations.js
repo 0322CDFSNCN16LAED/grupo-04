9165aa3e8730c4ed879b4dd3f8a9964dc61c948a
“juasru” <juliasisrueck@hotmail.com>
2022-09-06 20:15:17 -0300

    agregué validaciones de cantidad de caracteres en budget request

--- src/middlewares/budgetValidations.js
+++ src/middlewares/budgetValidations.js
@@ -6,16 +6,19 @@ module.exports = {
   budgReqValidations: [
     body("tituloSolicitud")
       .notEmpty()
-      .withMessage("Debes introducir un titulo de tu solicitud"),
+      .withMessage("Debes introducir un titulo de tu solicitud")
+      .isLength({ min: 5 })
+      .withMessage("El título de la solicitud debe tener al menos cinco caracteres"),
     body("detalleSolicitud")
       .notEmpty()
-      .withMessage("Debes detallar la solicitud que quieres realizar"),
+      .withMessage("Debes detallar la solicitud que quieres realizar")
+      .isLength({ min: 20 })
+      .withMessage("El detalle de la solicitud debe tener al menos veinte caracteres"),
     body("rubroNombre").notEmpty().withMessage("debes selccionar un rubro"),
     body("urgenciaTrabajo").notEmpty().withMessage("debes selccionar un campo"),
     body("ubicacion").notEmpty().withMessage("debes introducir una ubicación"),
     body("imgReferencia").custom((value, { req }) => {
       const file = req.files;
-
       const acceptedExtensions = [".gif", ".png", ".tif", ".jpg"];
 
       if (!file[0]) {
