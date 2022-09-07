d83942bd67c405a6aed2ec5581d9f23951ed5ccc
PieroJacinto <pierojacinto@gmail.com>
2022-09-07 19:20:31 -0300

    problem solved

--- src/middlewares/userLoggedMiddleware.js
+++
@@ -1,7 +0,0 @@
-function userLoggedMiddleware (req,res,next) {
-    res.locals.isLogged = req.session && req.session.userLogged;
-    res.locals.userLogged = (req.session && req.session.userLogged) ? req.session.userLogged : undefined,
-    next();
-}
-
-module.exports = userLoggedMiddleware;
