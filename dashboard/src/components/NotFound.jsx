f302343d1f762785d1610c54117b2ff6319b84b7
PieroJacinto <pierojacinto@gmail.com>
2022-09-11 00:17:40 -0300

    images deleted and 404 not fond image added

--- dashboard/src/components/NotFound.jsx
+++ dashboard/src/components/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import imagenFondo from "../assets/images/404.png";
+import imagenFondo from "../assets/images/404.jpg";
 
 function NotFound() {
   return (
@@ -9,7 +9,7 @@ function NotFound() {
         className="img-fluid px-3 px-sm-4 mt-3 mb-4"
         style={{ width: 30 + "rem" }}
         src={imagenFondo}
-        alt=" Star Wars - Mandalorian "
+        alt=" ReparaYa Not Found "
       />
     </div>
   );
