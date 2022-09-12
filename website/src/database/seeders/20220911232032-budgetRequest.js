b90068084b331c33579245859e976fa880b09e2e
PieroJacinto <pierojacinto@gmail.com>
2022-09-11 22:55:58 -0300

    budget request seeders fixed

--- website/src/database/seeders/20220911232032-budgetRequest.js
+++ website/src/database/seeders/20220911232032-budgetRequest.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 const { faker } = require("@faker-js/faker");
 const db = require("../models");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const budgetReqs = [];
     const budgReqImgs = [];
     const Rubros = await db.Rubro.findAll();
     const Users = await db.User.findAll({
-      where: { isProf: 0 }
+      where: { isProf: 0 },
     });
     const ReqImgsArray = [
       "imgref-1659569610773.jpg",
@@ -37,26 +37,33 @@ module.exports = {
       "imgref-1659814137833.jpg",
       "imgref-1659814137854.jpg",
       "imgref-1659901881187.jpg",
-      "imgref-1659901881218.jpg"
+      "imgref-1659901881218.jpg",
     ];
-    const UrgTrabajoArray = ["Lo antes posible","Esta semana","Este mes"];
+    const UrgTrabajoArray = ["Lo antes posible", "Esta semana", "Este mes"];
 
     Array(20)
       .fill(0)
       .forEach((_, i) => {
         const randomUserId = Users[Math.floor(Math.random() * Users.length)];
         const randomRubro = Rubros[Math.floor(Math.random() * Rubros.length)];
-        const randomUrgTrabajo = UrgTrabajoArray[Math.floor(Math.random() * UrgTrabajoArray.length)];
-        const randomImg = ReqImgsArray[Math.floor(Math.random() * ReqImgsArray.length)];
+        const randomUrgTrabajo =
+          UrgTrabajoArray[Math.floor(Math.random() * UrgTrabajoArray.length)];
+        const randomImg =
+          ReqImgsArray[Math.floor(Math.random() * ReqImgsArray.length)];
 
         const randomBudgReq = {
           id: i + 1,
           tituloSolicitud: faker.lorem.words(5),
-          userId: randomUserId,
-          rubroNombre: randomRubro,
+          userId: randomUserId.id,
+          rubroNombre: randomRubro.nombre,
           detalleSolicitud: faker.lorem.paragraphs(3),
           urgenciaTrabajo: randomUrgTrabajo,
-          ubicacion: faker.address.streetAddress() + ", " + faker.address.city() + ", " + faker.address.state(),
+          ubicacion:
+            faker.address.streetAddress() +
+            ", " +
+            faker.address.city() +
+            ", " +
+            faker.address.state(),
           estado: "PRESUPUESTO ENVIADO",
           created_at: new Date(),
           updated_at: new Date(),
@@ -67,14 +74,14 @@ module.exports = {
           img: randomImg,
           created_at: new Date(),
           updated_at: new Date(),
-        }); 
+        });
       });
     await queryInterface.bulkInsert("budget_request", budgetReqs);
     await queryInterface.bulkInsert("req_imgs", budgReqImgs);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("req_imgs", null, {});
     await queryInterface.bulkDelete("budget_request", null, {});
-  }
+  },
 };
