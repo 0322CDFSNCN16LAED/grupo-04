71279574dfdff5e497152f6b276fa0f968ee26cc
PieroJacinto <pierojacinto@gmail.com>
2022-09-13 11:27:33 -0300

    budget response seeders added

--- website/src/database/seeders/20220913015151-response-pintor.js
+++ website/src/database/seeders/20220913015151-response-pintor.js
@@ -5,14 +5,10 @@ const db = require("../models");
 module.exports = {
   async up(queryInterface, Sequelize) {
     const budgetRes = [];
-    const requestPintor = await db.budgReq.findAll({
-      where: {
-        rubroNombre: "Pintor",
-      },
-    });
+    const requestPintor = await db.budgReq.findAll();
     const Users = await db.User.findAll({
       where: { isProf: 1 },
-      include: [{ association: "rubros", where: { nombre: "Pintor" } }],
+      include: ["rubros"],
     });
     Array(20)
       .fill(0)
@@ -20,27 +16,27 @@ module.exports = {
         const randomUserId = Users[Math.floor(Math.random() * Users.length)];
         const randomRequestPintor =
           requestPintor[Math.floor(Math.random() * requestPintor.length)];
-
-        const randomBudgRes = {
-          id: i + 1,
-          reqId: randomRequestPintor.id,
-          userId: randomUserId.id,
-          materiales: faker.lorem.paragraphs(3),
-          precioMateriales: faker.datatype.number({ min: 8000, max: 80000 }),
-          manoDeObra: faker.lorem.paragraphs(1),
-          precioManoDeObra: faker.datatype.number({ min: 8000, max: 80000 }),
-          duracionTrabajo: faker.datatype.number({ min: 10, max: 50 }),
-          comentariosTrabajo: faker.lorem.paragraphs(2),
-          precioFinal: faker.datatype.number({ min: 8000, max: 80000 }),
-          estado: "PRESUPUESTO RESPONDIDO",
-          created_at: new Date(),
-          updated_at: new Date(),
-        };
-        budgetRes.push(randomBudgRes);
+        if (randomUserId.rubros[0].nombre == randomRequestPintor.rubroNombre) {
+          const randomBudgRes = {
+            id: i + 1,
+            reqId: randomRequestPintor.id,
+            userId: randomUserId.id,
+            materiales: faker.lorem.paragraphs(3),
+            precioMateriales: faker.datatype.number({ min: 8000, max: 80000 }),
+            manoDeObra: faker.lorem.paragraphs(1),
+            precioManoDeObra: faker.datatype.number({ min: 8000, max: 80000 }),
+            duracionTrabajo: faker.datatype.number({ min: 10, max: 50 }),
+            comentariosTrabajo: faker.lorem.paragraphs(2),
+            precioFinal: faker.datatype.number({ min: 80000, max: 200000 }),
+            estado: "PRESUPUESTO RESPONDIDO",
+            created_at: new Date(),
+            updated_at: new Date(),
+          };
+        }
+        console.log(JSON.stringify(budgetRes, null, 4));
       });
     await queryInterface.bulkInsert("budget_response", budgetRes);
   },
-
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("budget_response", null, {});
   },
