6438bf8b05693572da7db2f34115b0988639da3a
PieroJacinto <pierojacinto@gmail.com>
2022-09-13 20:35:47 -0300

    shopping-cart seeders

---
+++ website/src/database/seeders/20220913233117-shopping-cart.js
@@ -0,0 +1,46 @@
+"use strict";
+const { faker } = require("@faker-js/faker");
+const db = require("../models");
+const dayjs = require("dayjs");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const cart = [];
+    const responses = await db.budgRes.findAll({
+      include: ["budget_request"],
+    });
+
+    const estado = [
+      "TRABAJO AGENDADO",
+      "TRABAJO CONFIRMADO",
+      "TRABAJO CANCELADO",
+    ];
+    const metodoPago = ["efectivo", "debito", "credito", "mercadoPago"];
+    Array(100)
+      .fill(0)
+      .forEach((_, i) => {
+        const randomResponses =
+          responses[Math.floor(Math.random() * responses.length)];
+        const randomEstado = estado[Math.floor(Math.random() * estado.length)];
+        const randomPago =
+          metodoPago[Math.floor(Math.random() * metodoPago.length)];
+
+        const randomCart = {
+          id: i + 1,
+          resId: randomResponses.id,
+          userId: randomResponses.budget_request.userId,
+          dia: dayjs(faker.date.soon(10)).format("YYYY-MM-DD"),
+          horario: faker.datatype.datetime(),
+          metodoPago: randomPago,
+          estado: randomEstado,
+          created_at: new Date(),
+          updated_at: new Date(),
+        };
+        cart.push(randomCart);
+      });
+    await queryInterface.bulkInsert("shopping_cart", cart);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("shopping_cart", null, {});
+  },
+};
